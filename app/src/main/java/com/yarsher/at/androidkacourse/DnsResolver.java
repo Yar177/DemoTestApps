b9d290154eb337d58939d00321304bd9c232194e
Yar <yarsher.dev@gmail.com>
2019-03-04 13:51:49 -0500

    dnsResolver init

---
+++ app/src/main/java/com/yarsher/at/androidkacourse/DnsResolver.java
@@ -0,0 +1,38 @@
+package com.yarsher.at.androidkacourse;
+
+import android.util.Log;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class DnsResolver implements Runnable{
+
+    private String domain;
+    private InetAddress inetAddr;
+
+    private static final String TAG = "DnsResolver";
+
+    public DnsResolver(String domain){
+        this.domain = domain;
+    }
+
+
+
+    @Override
+    public void run() {
+        try {
+            InetAddress address = InetAddress.getByName(domain);
+            set(address);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            Log.e(TAG, "UnknownHostException", e);
+
+        }
+    }
+
+    private void set(InetAddress address) {
+        this.inetAddr = inetAddr;
+    }
+
+
+}
--- app/src/main/java/com/yarsher/at/androidkacourse/MainActivity.java
+++ app/src/main/java/com/yarsher/at/androidkacourse/MainActivity.java
@@ -1,13 +1,39 @@
 package com.yarsher.at.androidkacourse;
 
+import android.os.Build;
+import android.security.NetworkSecurityPolicy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    TextView textView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.tvText);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            boolean ctOn = NetworkSecurityPolicy.getInstance().isCleartextTrafficPermitted();
+
+            Log.i(TAG, "NetworkSecurityPolicy.getInstance().isCleartextTrafficPermitted(): " + ctOn);
+
+
+            textView.setText("NetworkSecurityPolicy.getInstance().isCleartextTrafficPermitted(): " + ctOn);
+
+        }
+
+
     }
 }
